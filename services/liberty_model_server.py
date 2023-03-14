5534ef0c1a59795c3ede6823814e706ef1994381
David Lanzendörfer <leviathan@libresilicon.com>
2023-03-14 22:07:04 +0000

    OK... maybe this way

--- services/liberty_model_server.py
+++ services/liberty_model_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from gevent.pywsgi import WSGIServer
 import os
+import threading
 
 from transformers import LLaMATokenizer, LLaMAForCausalLM, pipeline
 
@@ -9,7 +10,7 @@ from LibertyAI import get_configuration
 app = Flask(__name__)
 
 @app.route('/generation', methods=['POST'])
-def chat():
+def generation():
     text = request.form['input']
     key = request.form['API_KEY']
     try:
@@ -17,14 +18,11 @@ def chat():
     except:
         temp = 0
 
-    if key == API_KEY:
-        if BUSY_STATE:
-            return {'busy': "Please try again later"}
-        else:
-            BUSY_STATE = True
-            response = pipe(text, temperature=int(temp))
-            BUSY_STATE = False
-            return {'response': response}
+    if key == os.environ['LIBERTYAI_API_KEY']:
+        sem.acquire()
+        response = pipe(text, temperature=int(temp))
+        sem.release()
+        return {'response': response}
     else:
         return {'error': "Invalid API key"}
 
@@ -44,8 +42,7 @@ if __name__ == '__main__':
         tokenizer=tokenizer,
         max_new_tokens=100,
     )
-    API_KEY = os.environ['LIBERTYAI_API_KEY']
-    BUSY_STATE = False
+    sem = threading.Semaphore()
     http_server = WSGIServer(('', int(config.get('DEFAULT', 'ModelServicePort'))), app)
     http_server.serve_forever()
 
