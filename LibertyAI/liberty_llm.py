f87baaa28af1c5ee4666d920c94152ccd83c77ff
David Lanzendörfer <leviathan@libresilicon.com>
2023-03-15 13:51:23 +0000

    Handling JSON

--- LibertyAI/liberty_llm.py
+++ LibertyAI/liberty_llm.py
@@ -1,18 +1,37 @@
 from langchain.llms.base import LLM
 from typing import Optional, List, Mapping, Any
+import requests
+import os
 
 class LibertyLLM(LLM):
 
-    n: int
+    endpoint: str
+
+    json_headers = {
+        'content-type': 'application/json',
+    }
 
     @property
     def _llm_type(self) -> str:
-        return "custom"
+        return "liberty"
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-        if stop is not None:
-            raise ValueError("stop kwargs are not permitted.")
-        return prompt[:self.n]
+        #if stop is not None:
+        #    raise ValueError("stop kwargs are not permitted.")
+        json_data = {
+            'input' : prompt,
+            'temperature' : 0.4,
+            'API_KEY': os.environ['LIBERTYAI_API_KEY'],
+            'max_new_tokens': 20,
+        }
+        print('json_data',json_data)
+        response = requests.post(
+            self.endpoint,
+            json = json_data,
+        )
+        reply = response.json()
+
+        return reply['generated_text']
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
