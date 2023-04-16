d55c90f91c26cbcd6995caf311bba1cefb770946
David Lanzendörfer <leviathan@libresilicon.com>
2023-04-16 16:17:47 +0100

    Oops

--- services/liberty_flask_chatbot.py
+++ services/liberty_flask_chatbot.py
@@ -235,6 +235,5 @@ if __name__ == "__main__":
     emb = liberty_embedding()
     active_bots = {}
     active_conversations = {}
-    #app.run(host='0.0.0.0', port=5000)
-    http_server = WSGIServer('', 5000, app)
+    http_server = WSGIServer(('',5000), app)
     http_server.serve_forever()
