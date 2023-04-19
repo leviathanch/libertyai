7a731efd9ee13807f773b6585a64de83ed13f89b
David Lanzendörfer <leviathan@libresilicon.com>
2023-04-19 11:18:43 +0100

    Debugging

--- api_server/apiserver.cpp
+++ api_server/apiserver.cpp
@@ -180,7 +180,7 @@ int predict_text(
                     }
                 }
                 if ( params.verbosity_level > 0 ) {
-                    std::cout << tok << std::flush;
+                    std::cerr << tok << std::flush;
                 }
             }
             if(contains_stop(generated_text, params.stop)) {
