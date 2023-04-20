0ff12aa815c4f95828e6bbb910a5b9b6737a1057
David Lanzendörfer <leviathan@libresilicon.com>
2023-04-20 12:22:46 +0100

    Add a delay

--- services/liberty_flask_chatbot.py
+++ services/liberty_flask_chatbot.py
@@ -218,7 +218,9 @@ def chatbot_stream():
         token = ""
         while token != "[DONE]":
             token = bot.get_part(uuid, index)
-            if token != "[BUSY]":
+            if token == "[BUSY]":
+                time.sleep(1)
+            else:
                 index += 1
                 yield 'data: {}\n\n'.format(token)
 
